e6619354cf4cfc8dcd1b19382b021f7f399eeb07
Sahishnu <sahishnut@gmail.com>
2020-04-29 20:38:46 +0530

    Game Added

---
+++ src/components/PlayAgain.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const PlayAgain = props => (
+	<div className="game-done">
+  	<div 
+    	className="message"
+      style={{ color: props.gameStatus === 'lost' ? 'red' : 'green'}}
+    >
+  	  {props.gameStatus === 'lost' ? 'Game Over' : 'Nice'}
+  	</div>
+	  <button onClick={props.onClick}>Play Again</button>
+	</div>
+);
+
+export default function App(){
+    return <PlayAgain />
+}
